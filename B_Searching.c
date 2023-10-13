583ff8fb43c908c79c0bc1cd2187fe4d3d89ef93
ifty03 <ashikulislamb84@gmail.com>
2023-10-13 10:34:10 +0600

    searching problem solved

--- B_Searching.c
+++ B_Searching.c
@@ -1,6 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int n;
+    int x;
+    scanf("%d", &n);
+    int ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+    scanf("%d", &x);
+    for (int i = 0; i < n; i++)
+    {
+        if (ar[i] == x)
+        {
+            printf("%d", i);
+            break;
+        }
+        else
+        {
+            if(i==n-1){
+            printf("-1");
+            }
+        }
+    }
     return 0;
 }
