4b85ae969ee18f1756864ed9f8ce6aee6d40857e
ifty03 <ashikulislamb84@gmail.com>
2023-10-30 19:00:11 +0600

    solve

---
+++ Is_Palindrome.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+int is_palindrome(char *S)
+{
+    int len = strlen(S);
+    char c[1001];
+    strcpy(c, S);
+    for (int i = 1; i <= len; i++)
+    {
+        int l = 1;
+        int r = len - i;
+        if (l == r)
+        {
+            break;
+        }
+        else
+        {
+
+            char t = c[i - 1];
+            c[i - 1] = c[r];
+            c[r] = t;
+            l++;
+            r--;
+        }
+    }
+    int cmp = strcmp(S, c);
+    return cmp;
+}
+int main()
+{
+    char S[1001];
+    gets(S);
+
+    int cmp = is_palindrome(S);
+    if (cmp == 0)
+    {
+        printf("Palindrome\n");
+    }
+    else
+    {
+        printf("Not Palindrome\n");
+    }
+
+    return 0;
+}
